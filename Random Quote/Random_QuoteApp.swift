58510d86275d066fdb5170e261cef7dd0cf6e209
Usama Azam <usamaazam10@gmail.com>
2024-11-19 15:30:46 +0500

    initial commit with iOS project initialised

---
+++ Random Quote/Random_QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_QuoteApp.swift
+//  Random Quote
+//
+//  Created by Usama Azam on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Random_QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
